03e1d3b1766cc113d7df89eb7dbc1d78f26f6fb2
Harshit Gupta <harshit.11235@gmail.com>
2020-09-29 22:10:55 +0530

    New question-find divisors

---
+++ Number Theory/Questions/find_divisors.cpp
@@ -0,0 +1,116 @@
+#include<bits/stdc++.h>
+#include<cmath>
+#define tr(v,it) for(typeof(v.begin()) it = v.begin() ;it!=v.end();it++)
+#define in(x) lli x; cin>>x;
+#define all(v) v.begin(),v.end()
+#define for0(n) for(lli i=0;i<n;i++)
+#define for1(n) for(lli i=1;i<=n;i++)
+#define mod 1000000007
+#define asc_ord_set tree< lli , null_type,less<lli>, rb_tree_tag,tree_order_statistics_node_update>
+#define desc_ord_set tree< lli , null_type, greater<lli>, rb_tree_tag,tree_order_statistics_node_update>
+#define key_position(k,o) o.order_of_key(k)
+// Common file
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef long long int lli;
+typedef vector<lli> vi;
+typedef vector<vi> vvi;
+typedef unordered_map <lli,lli> umap;
+typedef priority_queue<lli> pq;
+typedef pair <lli, lli> pi;
+bitset<10000000> p;
+bool sieve_made = false;
+lli fast_power(lli a,lli b)
+{
+    lli p = a;
+    lli res = 1;
+    while(b>0)
+    {
+        if(b&1)
+            res*= p;
+        p= p*p;
+        b= b>>1;
+    }
+    return res;
+}
+void sieve()
+{   sieve_made=true;
+    p[0] = p[1] = 0;
+    p[2] = 1;
+    for(lli i=4;i<=1000000;i+=2)
+        p[i] = 0;
+    for(lli i=3;i<=1000000;i+=2)
+    {
+        if(p[i])
+        {
+            for(lli j=i*i;j<=1000000;j+=i)
+                p[j] = 0;
+        }
+    }
+}
+bool prime(lli n)
+{
+    if(sieve_made==true)
+    {
+        if(n<10000000)
+            return (p[n]);
+        else{
+             for(int i=2;i<=sqrt(n);i++)
+                    if(n%i==0) return false;
+            return true;
+        }
+    }
+    for(int i=2;i<=sqrt(n);i++)
+        {
+            if(n%i==0) return false;
+        }
+    return true;
+}
+map <lli,lli> factor;
+void prime_fact(lli n)
+{
+    for(int i=2;i<=int(sqrt(n));i++)
+    {
+        if(n%i==0)
+        {
+            while(n%i==0)
+                {
+                    factor[i]++;
+                    n = n/i;
+                }
+        }
+    }
+    if(n!=1) factor[n] += 1;
+}
+// key idea -> factor map gives you all the prime factors of the number a1*a2*a3*a4...
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        lli arr[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        for(auto k:arr)
+        {
+            // find factors of all in factor map.
+            prime_fact(k);
+            // this keeps on storing the prime factor
+            // powers in the factor map.
+        }
+        lli res = 1;
+        for(auto x:factor)
+        {
+            res = (res%mod * ((x.second)+1)%mod)%mod;
+        }
+        cout<<res<<endl;
+        factor.clear();
+
+    }
+    return 0;
+}
