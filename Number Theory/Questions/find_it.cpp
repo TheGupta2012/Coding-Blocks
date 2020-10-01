e27bd9144489140525c990f205a55b2cd429fab8
Harshit Gupta <harshit.11235@gmail.com>
2020-10-01 22:47:18 +0530

    New question - find it

--- Number Theory/Questions/find_it.cpp
+++ Number Theory/Questions/find_it.cpp
@@ -2,47 +2,48 @@
 using namespace std;
 typedef long long int lli;
 vector<lli> primes;
-bitset<100000> b;
-void sieve()
+bitset<100005> b;
+void sieve(lli *sums,unordered_map<lli,lli> &count)
 {
-    for(lli i=0;i<=100000;i++)
-        b[i] = 1;
-
-    //mark all as prime
-    for(lli i=4;i<=100000;i+=2)
-        b[i] = 0; // all even as non prime
-    b[2] = 1;
-    b[0] = b[1] = 0;
-
-    for(lli i=2;i<=100000;i++)
+    for(lli i=2;i<=100009;i++)
     {
-        if(b[i]==1)
-        {   primes.push_back(i);
-            for(lli j=2*i;j<=100000;j+=i)
-                b[j] = 0;
+            // why are we doing this?
+            // sums[i] is the count of the no of multiples of i.
+            // but j is actually a multiple of i, so we always
+            // check whether that number is actually a number
+            // in our frequency array and thus we increment the
+            // multiples of i in array a by its frequency(count[j]).
+            for(lli j=i;j<=100009;j+=i)
+                {
+                if (count.find(j)!=count.end())
+                    sums[i]+= count[j];//heart of the problem
+                }
         }
-    }
+
 }
 int main()
-{   sieve();
+{
     lli n,q,k;
     cin>>n;
     lli arr[n];
-    lli max = -1;
+    unordered_map<lli,lli> count;
     for(int i=0;i<n;i++)
-        {cin>>arr[i];
-        if(arr[i]>max) max= arr[i];
-    }
-    
+        {
+        cin>>arr[i];
+        count[arr[i]]++;}
+    lli sums[100011] = {0};
+    sieve(sums,count);
     //let's construct an array stating how many times
     // a number appears as a divisor for any of the
     // number in the array.
-    unordered_map <lli,lli> appear;
+
     cin>>q;
     while(q--)
     {
         cin>>k;
-        cout<<appear[k]<<endl;
+        if(k==1) {cout<<n<<endl;
+        continue;}
+        cout<<sums[k]<<endl;
     }
     return 0;
 }
