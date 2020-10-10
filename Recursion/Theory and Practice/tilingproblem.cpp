0edc29a11cb4a303fc999641ee923851a62cea97
Harshit Gupta <harshit.11235@gmail.com>
2020-10-10 15:25:31 +0530

    Tiling problem

---
+++ Recursion/Theory and Practice/tilingproblem.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout<<"A tiling problem\n";
+    cout<<" Given a wall of size 4 x n and tiles of size\n 1x4 and 4x1 , find how many ways you\n can build the wall?";
+    cout<<"Enter n:";
+    int n;
+    cin>>n;
+
+    return 0;
+}
