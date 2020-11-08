3c93976ac72e5c8a6a1491e6052db2f0960ea3da
Harshit Gupta <harshit.11235@gmail.com>
2020-11-08 22:54:19 +0530

    new question

---
+++ Divide And Conquer/Questions/alpha_score.cpp
@@ -0,0 +1,113 @@
+#include<bits/stdc++.h>
+#include<cmath>
+#define tr(v,it) for(typeof(v.begin()) it = v.begin() ;it!=v.end();it++)
+#define in(x) lli x; cin>>x
+#define all(v) v.begin(),v.end()
+#define for0(n) for(lli i=0;i<n;i++)
+#define loop(x,n,m) for(lli i=x;i<n;i+=m)
+#define mod 1000000007
+#define asc_ord_set tree< lli , null_type,less<lli>, rb_tree_tag,tree_order_statistics_node_update>
+#define desc_ord_set tree< lli , null_type, greater<lli>, rb_tree_tag,tree_order_statistics_node_update>
+#define key_position(k,o) o.order_of_key(k)
+// Common file
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef long long int lli;
+typedef vector<lli> vi;
+typedef vector<vi> vvi;
+typedef unordered_map <lli,lli> umap;
+typedef priority_queue<lli> pq;
+typedef pair <lli, lli> pi;
+bitset<10000000> p;
+bool sieve_made = false;
+lli fast_power(lli a,lli b)
+{
+    lli p = a;
+    lli res = 1;
+    while(b>0)
+    {
+        if(b&1)
+            res*= p;
+        p= p*p;
+        b= b>>1;
+    }
+    return res;
+}
+void sieve()
+{   sieve_made=true;
+    p[0] = p[1] = 0;
+    p[2] = 1;
+    for(lli i=4;i<=1000000;i+=2)
+        p[i] = 0;
+    for(lli i=3;i<=1000000;i+=2)
+    {
+        if(p[i])
+        {
+            for(lli j=i*i;j<=1000000;j+=i)
+                p[j] = 0;
+        }
+    }
+}
+bool prime(lli n)
+{
+    if(sieve_made==true)
+    {
+        if(n<10000000)
+            return (p[n]);
+        else{
+             for(int i=2;i<=sqrt(n);i++)
+                    if(n%i==0) return false;
+            return true;
+        }
+    }
+    for(int i=2;i<=sqrt(n);i++)
+        {
+            if(n%i==0) return false;
+        }
+    return true;
+}
+map <lli,lli> factor;
+void prime_fact(lli n)
+{
+    for(int i=2;i<=int(sqrt(n));i++)
+    {
+        if(n%i==0)
+        {
+            while(n%i==0)
+                {
+                    factor[i]++;
+                    n = n/i;
+                }
+        }
+    }
+    if(n!=1) factor[n] = 1;
+}
+// order n^2.
+lli calc_sum(lli i, lli n, lli *arr){
+    lli res=0;
+    for(int k=0;k<i;k++){
+        if(arr[k]<arr[i])
+            res = (res%mod + arr[k]%mod)%mod;
+    }
+    //cout<<"Result for i:"<<i<<":"<<res<<endl;
+    return res;
+}
+// order log(n)
+lli cal_sum_binsearch(lli i,lli n,lli *arr){
+
+}
+int main(){
+    ios::sync_with_stdio(0);
+    in(n);
+    lli arr[n];
+    for0(n)
+        cin>>arr[i];
+    lli total = 0;
+    loop(1,n,1){
+        total = (total%mod + calc_sum(i,n,arr)%mod)%mod;
+    }
+    cout<<total;
+    return 0;
+}
